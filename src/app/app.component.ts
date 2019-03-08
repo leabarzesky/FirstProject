99c702a8d0b04b8cf753d19b4525dfcbdeac7355
leabarzesky <leabarzesky@gmail.com>
2019-03-08 19:40:30 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  courses = COURSES;
+
+  title = COURSES[0].description;
+price = 9.99;
+rate  = 0.67;
+
+course = COURSES[0];
+  startDate = new Date(2000,0,1);
+
+
+
+  onCourseSelected(course: Course) {
+   console.log("app Component - click envent bubbled", course);
+
+  }
+}
